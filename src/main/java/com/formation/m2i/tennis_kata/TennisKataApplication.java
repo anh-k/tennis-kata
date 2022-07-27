9e74a10683871dc910344ce56230f9b4c6b14d70
Anh-Khoa Huynh <khoakun@gmail.com>
2022-07-27 14:05:04 +0200

    init project

---
+++ src/main/java/com/formation/m2i/tennis_kata/TennisKataApplication.java
@@ -0,0 +1,13 @@
+package com.formation.m2i.tennis_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formation/m2i/tennis_kata/TennisKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.m2i.tennis_kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
